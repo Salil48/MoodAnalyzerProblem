51de4c4421e5c0947a7b4397baa8195babae377a
“Salil48” <salilchharia@gmail.com>
2023-01-17 20:30:27 +0530

    Welcome to Mood Analyzer Problem

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodAnalyserProb
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyser Problem!");
+        }
+
+    }
+}
